0d7a2490169a68ccfe62783c516b243fcfde70a7
Nalendrooo <nalendroadil274@gmail.com>
2025-03-19 14:15:22 +0700

    pos

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4173, host: true,
+
+  },
+  preview: {
+    port: 4173,
+    host: true, // Pastikan host diatur ke true atau string yang sesuai
+    allowedHosts: true,
+  },
 })
