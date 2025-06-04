add4f0782d0cf7cb8a9a0675a7431e9affe03b48
Nalendrooo <nalendroadil274@gmail.com>
2025-06-04 10:09:50 +0700

    d

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,20 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Pisahkan vendor libraries
+          vendor: ['react', 'react-dom'],
+          // Pisahkan router jika menggunakan
+          router: ['react-router-dom'],
+        }
+      }
+    },
+    // Set chunk size warning limit
+    chunkSizeWarningLimit: 1000,
+  },
   server: {
     port: 4000, host: true,
 
