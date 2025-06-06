4f547e0267cc890ac1d5089829c7f91ee16c3ef9
Nalendrooo <nalendroadil274@gmail.com>
2025-06-06 22:21:09 +0700

    p

--- src/feature/_global/component/Footer/Footer.jsx
+++ src/feature/_global/component/Footer/Footer.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div className='bg-[#6148FF] p-10 flex gap-4 justify-center'>
-            <div>
-                <img src='/logo_smk_maarif.png' alt="logo" style={{ width: "80px" }} />
-            </div>
-            <div className='text-white'>
-                <p className='font-bold text-xl'>SMK MAARIF NU Margasari</p>
-                <p>JL. Raya Selatan Margasari, Desa Margasari</p>
-                <p>Kabupaten Tegal, 52463</p>
-            </div>
+  const location = useLocation();
 
-        </div>
-    )
-}
+  // Cek apakah path-nya adalah /siswa
+  const isSiswaPage = location.pathname === "/siswa";
 
-export default Footer
+  if (isSiswaPage) {
+    return null; // Sembunyikan footer hanya di halaman /siswa
+  }
+
+  return (
+    <div className="bg-[#6148FF] p-10 flex gap-4 justify-center">
+      <div>
+        <img src="/logo_smk_maarif.png" alt="logo" style={{ width: "80px" }} />
+      </div>
+      <div className="text-white">
+        <p className="font-bold text-xl">SMK MAARIF NU Margasari</p>
+        <p>JL. Raya Selatan Margasari, Desa Margasari</p>
+        <p>Kabupaten Tegal, 52463</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
